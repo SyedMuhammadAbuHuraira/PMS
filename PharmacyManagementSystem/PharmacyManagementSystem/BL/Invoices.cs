84dab87fe19259b58efd4f578d2aec761842306d
Burhan Haseeb <burhan@gmail.com>
2023-10-21 20:32:47 +0500

    BACKEND OF INVOICE ORDER

---
+++ PharmacyManagementSystem/PharmacyManagementSystem/BL/Invoices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagementSystem.BL
+{
+    internal class Invoices
+    {
+        public string Date;
+        public InvoiceOrder Orders;
+        public string loggedUser;
+        public string totalDiscount;
+        public string GrandTotal;
+    }
+}
