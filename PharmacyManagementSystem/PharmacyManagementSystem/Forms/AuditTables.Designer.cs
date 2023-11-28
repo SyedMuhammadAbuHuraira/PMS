1f33704e110e7e134d29b8d7317e206f7708ab88
Burhan Haseeb <burhan@gmail.com>
2023-11-28 18:36:25 +0500

    Audit Tables ( Patient , User , Manufacturer , Supplier , Person)

---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/AuditTables.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PharmacyManagementSystem.Forms
+{
+    partial class AuditTables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.iconButton4 = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.iconButton1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.iconButton4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(978, 556);
+            this.panel1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 30);
+            this.label2.TabIndex = 62;
+            this.label2.Text = "Audit Table";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 237);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 187);
+            this.dataGridView1.TabIndex = 61;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Patient-Audit",
+            "Supplier-Audit",
+            "Manufacture-Audit",
+            "User-Audit",
+            "Person-Audit"});
+            this.comboBox1.Location = new System.Drawing.Point(104, 152);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 32);
+            this.comboBox1.TabIndex = 73;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // iconButton4
+            // 
+            this.iconButton4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.iconButton4.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.iconButton4.FlatAppearance.BorderSize = 0;
+            this.iconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton4.IconChar = FontAwesome.Sharp.IconChar.Searchengin;
+            this.iconButton4.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton4.IconSize = 32;
+            this.iconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton4.Location = new System.Drawing.Point(326, 152);
+            this.iconButton4.Name = "iconButton4";
+            this.iconButton4.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.iconButton4.Size = new System.Drawing.Size(54, 31);
+            this.iconButton4.TabIndex = 72;
+            this.iconButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton4.UseVisualStyleBackColor = false;
+            this.iconButton4.Click += new System.EventHandler(this.iconButton4_Click);
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.iconButton1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Add;
+            this.iconButton1.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 32;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(492, 147);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.iconButton1.Size = new System.Drawing.Size(143, 36);
+            this.iconButton1.TabIndex = 74;
+            this.iconButton1.Text = "Show LOGS";
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // AuditTables
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 511);
+            this.Controls.Add(this.panel1);
+            this.Name = "AuditTables";
+            this.Text = "AuditTables";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/AuditTables.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmacyManagementSystem.Forms
+{
+    public partial class AuditTables : Form
+    {
+        public AuditTables()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void iconButton4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd4;
+           // try
+            //{
+
+
+                if (comboBox1.Text != "Patient-Audit" || comboBox1.Text != "User-Audit" || comboBox1.Text != "Manufacture-Audit" || comboBox1.Text != "Supplier-Audit" || comboBox1.Text != "Person-Audit")
+                {
+                    if (comboBox1.SelectedIndex == 0)
+                    {
+
+                        cmd4 = new SqlCommand("Select * From PatientAudit ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                                dataGridView1.Columns[5].ReadOnly = true;
+                                dataGridView1.Columns[6].ReadOnly = true;
+                               // dataGridView1.Columns[7].ReadOnly = true;
+
+                            }
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 1)
+                    {
+
+                        cmd4 = new SqlCommand("Select * From SupplierAudit ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                                //dataGridView1.Columns[5].ReadOnly = true;
+
+                            }
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 2)
+                    {
+
+                        cmd4 = new SqlCommand("Select * From ManufacturerAudit ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                                //dataGridView1.Columns[5].ReadOnly = true;
+
+                            }
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 3)
+                    {
+
+                        cmd4 = new SqlCommand("Select * From UsersAudit ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                                dataGridView1.Columns[5].ReadOnly = true;
+                                dataGridView1.Columns[6].ReadOnly = true;
+                                dataGridView1.Columns[7].ReadOnly = true;
+                                dataGridView1.Columns[8].ReadOnly = true;
+                                dataGridView1.Columns[9].ReadOnly = true;
+                                dataGridView1.Columns[10].ReadOnly = true;
+                                //dataGridView1.Columns[11].ReadOnly = true;
+
+                            }
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 4)
+                    {
+
+                        cmd4 = new SqlCommand("Select * From PersonAudit ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                                dataGridView1.Columns[5].ReadOnly = true;
+                                dataGridView1.Columns[6].ReadOnly = true;
+                                dataGridView1.Columns[7].ReadOnly = true;
+                                dataGridView1.Columns[8].ReadOnly = true;
+                                dataGridView1.Columns[9].ReadOnly = true;
+                                dataGridView1.Columns[10].ReadOnly = true;
+                                dataGridView1.Columns[11].ReadOnly = true;
+                                dataGridView1.Columns[12].ReadOnly = true;
+                                dataGridView1.Columns[13].ReadOnly = true;
+                               // dataGridView1.Columns[14].ReadOnly = true;
+
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Select the Correct One");
+                    return;
+                }
+           /* }
+            catch(Exception exp)
+            {
+                cmd4 = new SqlCommand("insert into LOGS values (@CreatedAt , @LogTitle , @LogClass , @LogFunction)", con);
+                cmd4.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+                cmd4.Parameters.AddWithValue("@LogTitle", exp.Message);
+                cmd4.Parameters.AddWithValue("@LogClass", "AuditTables");
+                cmd4.Parameters.AddWithValue("@LogFunction", "SearchButton");
+                cmd4.ExecuteNonQuery();
+            }*/
+          
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = null;
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd4;
+           // try
+            //{
+
+
+
+                        cmd4 = new SqlCommand("Select * From LOGS ", con);
+                        using (SqlDataReader reader = cmd4.ExecuteReader())
+                        {
+                            // Check if the reader has rows
+                            if (reader.HasRows)
+                            {
+                                // Create a DataTable to store the data
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+
+                                // Bind the DataTable to your grid
+                                dataGridView1.DataSource = dataTable;
+                                dataGridView1.Columns[1].ReadOnly = true;
+                                dataGridView1.Columns[2].ReadOnly = true;
+                                dataGridView1.Columns[3].ReadOnly = true;
+                                dataGridView1.Columns[4].ReadOnly = true;
+                              //  dataGridView1.Columns[5].ReadOnly = true;
+                               
+
+                            }
+                        }
+                    
+                   
+                    
+                   
+          /*  }
+            catch (Exception exp)
+            {
+                cmd4 = new SqlCommand("insert into LOGS values (@CreatedAt , @LogTitle , @LogClass , @LogFunction)", con);
+                cmd4.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+                cmd4.Parameters.AddWithValue("@LogTitle", exp.Message);
+                cmd4.Parameters.AddWithValue("@LogClass", "AuditTables");
+                cmd4.Parameters.AddWithValue("@LogFunction", "Show LOGS Button");
+                cmd4.ExecuteNonQuery();
+            }*/
+        }
+    }
+}
