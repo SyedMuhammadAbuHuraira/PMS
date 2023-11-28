605bce4b8fa496de2e92f63faf5c4baac21d2fad
Burhan Haseeb <burhan@gmail.com>
2023-11-28 18:37:10 +0500

    Dashboard Feature

---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/GenerateReports.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PharmacyManagementSystem.Forms
+{
+    partial class GenerateReports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.SalesReport = new FontAwesome.Sharp.IconButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            this.panelDesktop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panelDesktop);
+            this.panel1.Location = new System.Drawing.Point(-5, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(978, 556);
+            this.panel1.TabIndex = 9;
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.White;
+            this.panelDesktop.Controls.Add(this.iconButton1);
+            this.panelDesktop.Controls.Add(this.SalesReport);
+            this.panelDesktop.Controls.Add(this.label2);
+            this.panelDesktop.Controls.Add(this.label1);
+            this.panelDesktop.Controls.Add(this.dateTimePicker2);
+            this.panelDesktop.Controls.Add(this.dateTimePicker1);
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(0, 0);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(978, 556);
+            this.panelDesktop.TabIndex = 4;
+            // 
+            // SalesReport
+            // 
+            this.SalesReport.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SalesReport.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.SalesReport.FlatAppearance.BorderSize = 0;
+            this.SalesReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SalesReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesReport.ForeColor = System.Drawing.Color.Gainsboro;
+            this.SalesReport.IconChar = FontAwesome.Sharp.IconChar.Searchengin;
+            this.SalesReport.IconColor = System.Drawing.Color.Gainsboro;
+            this.SalesReport.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.SalesReport.IconSize = 32;
+            this.SalesReport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SalesReport.Location = new System.Drawing.Point(116, 199);
+            this.SalesReport.Name = "SalesReport";
+            this.SalesReport.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.SalesReport.Size = new System.Drawing.Size(113, 34);
+            this.SalesReport.TabIndex = 76;
+            this.SalesReport.Text = "Sales Report";
+            this.SalesReport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SalesReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SalesReport.UseVisualStyleBackColor = false;
+            this.SalesReport.Click += new System.EventHandler(this.SalesReport_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(455, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "TO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "FROM";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(458, 108);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 14;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(116, 108);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.iconButton1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Searchengin;
+            this.iconButton1.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 32;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(308, 199);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.iconButton1.Size = new System.Drawing.Size(113, 34);
+            this.iconButton1.TabIndex = 77;
+            this.iconButton1.Text = "Expiry Report";
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // GenerateReports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(977, 533);
+            this.Controls.Add(this.panel1);
+            this.Name = "GenerateReports";
+            this.Text = "GenerateReports";
+            this.panel1.ResumeLayout(false);
+            this.panelDesktop.ResumeLayout(false);
+            this.panelDesktop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelDesktop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private FontAwesome.Sharp.IconButton SalesReport;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/GenerateReports.cs
@@ -0,0 +1,226 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using OfficeOpenXml.Style;
+
+namespace PharmacyManagementSystem.Forms
+{
+    public partial class GenerateReports : Form
+    {
+        public GenerateReports()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+        private void SalesReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                {
+                    // Code to be executed only during design-time
+                }
+                else
+                {
+                    // Code to be executed during run-time
+                }
+
+                var con = Configuration.getInstance().getConnection();
+
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                DateTime startDate = dateTimePicker1.Value;
+                DateTime endDate = dateTimePicker2.Value;
+
+                // Construct the parameterized SQL query
+                SqlCommand cmd2 = new SqlCommand("SELECT \r\n    Products.Name, \r\n    Products.Type, \r\n    CAST(Products.RetailPrice / CAST(Products.ConversionalUnit AS decimal(5,2)) AS decimal(18,2)) AS ActualPrice, \r\n    CAST(Invoice.Payment AS decimal(18,2)) AS Payment, \r\n    CAST(Invoice.GrandTotal AS decimal(18,2)) AS GrandTotal, \r\n    CAST(Invoice.TotalDiscount AS decimal(18,2)) AS Discount, \r\n    CAST(Invoice.Total AS decimal(18,2)) AS Total \r\nFROM \r\n    Invoice \r\n    INNER JOIN Invoice_Order ON Invoice.InvoiceID = Invoice_Order.InvoiceOrderID \r\n    INNER JOIN Products ON Invoice_Order.ProductID = Products.ProductID \r\nWHERE \r\n    Invoice.InvoiceDate BETWEEN @StartDate AND @EndDate\r\n", con);
+                cmd2.Parameters.AddWithValue("@StartDate", startDate);
+                cmd2.Parameters.AddWithValue("@EndDate", endDate);
+
+                // Execute the query and create a DataTable to hold the results
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+               
+                // Create a new Excel workbook and worksheet
+                using (ExcelPackage excelPackage = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sales Report");
+                    // Set the big header "Sales Report"
+                    worksheet.Cells["A1"].Value = "Sales Report";
+                    worksheet.Cells["A1:H8"].Merge = true;
+                    worksheet.Cells["A1"].Style.Font.Size = 24;
+                    worksheet.Cells["A1"].Style.Font.Bold = true;
+                    worksheet.Cells["A1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    worksheet.Cells["A1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                    // Set the subtitle "Start Date - End Date"
+                    worksheet.Cells["A9"].Value = "Start Date:";
+                    worksheet.Cells["B9"].Value = startDate.ToString("yyyy-MM-dd");
+                    worksheet.Cells["C9"].Value = "End Date:";
+                    worksheet.Cells["D9"].Value = endDate.ToString("yyyy-MM-dd");
+
+                    // Add two empty rows
+                    worksheet.Cells["A10:H11"].Merge = true;
+
+                    // Write the column headers to the worksheet
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        worksheet.Cells[12, i].Value = dataTable.Columns[i - 1].ColumnName;
+                    }
+
+                    // Write the data rows to the worksheet
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dataTable.Columns.Count; j++)
+                        {
+                            worksheet.Cells[i + 13, j + 1].Value = dataTable.Rows[i][j].ToString();
+                        }
+                    }
+
+                    // Calculate the total sales
+                    decimal totalSales = 0;
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        totalSales += Convert.ToDecimal(row["Payment"]);
+                    }
+
+                    // Add an empty row
+                    worksheet.Cells[dataTable.Rows.Count + 14, 1].Value = "";
+
+                    // Write the total sales to the worksheet
+                    worksheet.Cells[dataTable.Rows.Count + 15, dataTable.Columns.Count - 1].Value = "Total Sales";
+                    worksheet.Cells[dataTable.Rows.Count + 15, dataTable.Columns.Count].Value = totalSales;
+
+                    // Auto-fit the columns
+                    worksheet.Cells.AutoFitColumns();
+                    // Save the Excel file to disk and open it
+                    string fileName = "SalesReport.xlsx";
+                    string savePath = @"C:\Users\MUHAMMAD BURHAN\OneDrive\Desktop\Semester 5\SE LAB\BURHAN\" + fileName;
+                    FileInfo fileInfo = new FileInfo(savePath);
+                    excelPackage.SaveAs(fileInfo);
+
+                    MessageBox.Show("Sales report generated successfully and saved as " + fileName);
+                    System.Diagnostics.Process.Start(savePath);
+                        
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while fetching the Sales Report: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                {
+                    // Code to be executed only during design-time
+                }
+                else
+                {
+                    // Code to be executed during run-time
+                }
+
+                var con = Configuration.getInstance().getConnection();
+
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                
+                DateTime startDate = dateTimePicker1.Value;
+                DateTime endDate = dateTimePicker2.Value;
+
+                // Construct the parameterized SQL query
+                SqlCommand cmd2 = new SqlCommand("SELECT \r\n    Products.Name, \r\n    Products.Type, \r\n    Products.Supplier as SupplierName, \r\n    StockItems.StockID, \r\n    StockItems.ExpiredDate \r\nFROM \r\n    StockItems \r\n    INNER JOIN Products ON Products.ProductID = StockItems.ProductID \r\nWHERE \r\n    StockItems.ExpiredDate BETWEEN @StartDate AND @EndDate\r\n", con);
+                cmd2.Parameters.AddWithValue("@StartDate", startDate);
+                cmd2.Parameters.AddWithValue("@EndDate", endDate);
+
+
+                // Execute the query and create a DataTable to hold the results
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+
+                // Create a new Excel workbook and worksheet
+                using (ExcelPackage excelPackage = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Dead Stock Report");
+                    // Set the big header "Sales Report"
+                    worksheet.Cells["A1"].Value = "DEAD STOCK REPROT";
+                    worksheet.Cells["A1:H8"].Merge = true;
+                    worksheet.Cells["A1"].Style.Font.Size = 24;
+                    worksheet.Cells["A1"].Style.Font.Bold = true;
+                    worksheet.Cells["A1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    worksheet.Cells["A1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                    // Set the subtitle "Start Date - End Date"
+                    worksheet.Cells["A9"].Value = "Start Date:";
+                    worksheet.Cells["B9"].Value = startDate.ToString("yyyy-MM-dd");
+                    worksheet.Cells["C9"].Value = "End Date:";
+                    worksheet.Cells["D9"].Value = endDate.ToString("yyyy-MM-dd");
+
+                    // Add two empty rows
+                    worksheet.Cells["A10:H11"].Merge = true;
+
+                    // Write the column headers to the worksheet
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        worksheet.Cells[12, i].Value = dataTable.Columns[i - 1].ColumnName;
+                    }
+
+                    // Write the data rows to the worksheet
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dataTable.Columns.Count; j++)
+                        {
+                            worksheet.Cells[i + 13, j + 1].Value = dataTable.Rows[i][j].ToString();
+                        }
+                    }
+
+                  
+
+                    // Add an empty row
+                    worksheet.Cells[dataTable.Rows.Count + 14, 1].Value = "";
+
+                   
+                    // Auto-fit the columns
+                    worksheet.Cells.AutoFitColumns();
+                    // Save the Excel file to disk and open it
+                    string fileName = "DeadStockReport.xlsx";
+                    string savePath = @"C:\Users\MUHAMMAD BURHAN\OneDrive\Desktop\Semester 5\SE LAB\BURHAN\" + fileName;
+                    FileInfo fileInfo = new FileInfo(savePath);
+                    excelPackage.SaveAs(fileInfo);
+
+                    MessageBox.Show("DeadStock report generated successfully and saved as " + fileName);
+                    System.Diagnostics.Process.Start(savePath);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while fetching the Sales Report: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
