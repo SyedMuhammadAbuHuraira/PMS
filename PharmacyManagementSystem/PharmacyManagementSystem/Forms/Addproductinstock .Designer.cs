6c3c13f10ca1cd141ee81755a736a12fbf7a7843
Burhan Haseeb <burhan@gmail.com>
2023-11-28 18:33:18 +0500

    Add Product In Stock

---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/Addproductinstock .Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace PharmacyManagementSystem.Forms
+{
+    partial class Add_product_in_stock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iconButton6 = new FontAwesome.Sharp.IconButton();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.iconButton6);
+            this.panel1.Controls.Add(this.textBox10);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 126);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(697, 190);
+            this.dataGridView1.TabIndex = 50;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // iconButton6
+            // 
+            this.iconButton6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.iconButton6.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.iconButton6.FlatAppearance.BorderSize = 0;
+            this.iconButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton6.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            this.iconButton6.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconButton6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton6.IconSize = 32;
+            this.iconButton6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton6.Location = new System.Drawing.Point(335, 80);
+            this.iconButton6.Name = "iconButton6";
+            this.iconButton6.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.iconButton6.Size = new System.Drawing.Size(53, 25);
+            this.iconButton6.TabIndex = 49;
+            this.iconButton6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton6.UseVisualStyleBackColor = false;
+            this.iconButton6.Click += new System.EventHandler(this.iconButton6_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.Location = new System.Drawing.Point(45, 80);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(223, 24);
+            this.textBox10.TabIndex = 47;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // label14
+            // 
+            this.label14.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(41, 58);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(283, 19);
+            this.label14.TabIndex = 48;
+            this.label14.Text = "Search Item By Name,Generic Name";
+            // 
+            // Add_product_in_stock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Add_product_in_stock";
+            this.Text = "Add_product_in_stock";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private FontAwesome.Sharp.IconButton iconButton6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label14;
+    }
+}
---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Forms/Addproductinstock .cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PharmacyManagementSystem.DL;
+using PharmacyManagementSystem.BL;
+
+namespace PharmacyManagementSystem.Forms
+{
+    public partial class Add_product_in_stock : Form
+    {
+        public Add_product_in_stock()
+        {
+            InitializeComponent();
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int counter = 0;
+                if (e.ColumnIndex == 13)
+                {
+
+                    if (counter == 0)
+                    {
+                        counter = 1;                        
+                        DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                        string Create = selectedRow.Cells[0].Value.ToString();
+                        string Update = selectedRow.Cells[1].Value.ToString();
+                        DateTime CreatedAt, UpdatedAt;
+                        // Attempt to parse the strings to DateTime
+                        if (DateTime.TryParse(Create, out CreatedAt)) { }
+                        else
+                        {
+                            MessageBox.Show("error in the converting CreatedAt string to datetime");
+                        }
+
+                        if (DateTime.TryParse(Update, out UpdatedAt)) { }
+                        else
+                        {
+                            MessageBox.Show("error in the converting UpdatedAt string to datetime");
+                        }
+
+                        string active = selectedRow.Cells[2].Value.ToString();
+                        string productID = selectedRow.Cells[3].Value.ToString();
+                        string Company = selectedRow.Cells[4].Value.ToString();
+                        string Supplier = selectedRow.Cells[5].Value.ToString();
+                        string Name = selectedRow.Cells[6].Value.ToString();
+                        string Type = selectedRow.Cells[7].Value.ToString();
+                        string Costprice = selectedRow.Cells[8].Value.ToString();
+                        string Retail = selectedRow.Cells[9].Value.ToString();
+                        string Margin = selectedRow.Cells[10].Value.ToString();
+                        string conversionalunit = selectedRow.Cells[11].Value.ToString();
+                        float costy = float.Parse(selectedRow.Cells[12].Value.ToString());
+                        string Sub_total = (costy * 1).ToString();
+                        Additemforstock addItem = new Additemforstock(CreatedAt, UpdatedAt, active, productID, Company, Supplier, Name, Type, Costprice, Retail, Margin, conversionalunit, Sub_total);
+                        AdditemforstockDL.addstock.Add(addItem);
+                        this.Close();
+                        // Check if the quantity is greater than 0 before proceeding with the addition of the item
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while fetching data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                SqlCommand cmd;
+                var con = Configuration.getInstance().getConnection();
+                cmd = new SqlCommand("insert into LOGS values (@CreatedAt , @LogTitle , @LogClass , @LogFunction)", con);
+                cmd.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+                cmd.Parameters.AddWithValue("@LogTitle", "AddProductInStocks _ DataGridViewCellClick");
+                cmd.Parameters.AddWithValue("@LogClass", "AddProductInStocks");
+                cmd.Parameters.AddWithValue("@LogFunction", "DataGridViewCellClick");
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                dataGridView1.Columns.Clear();
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                // Clear the grid before fetching new data
+                dataGridView1.DefaultCellStyle.SelectionBackColor = Color.FromArgb(30, 144, 255); // Set the selection color to a light blue shade
+
+                string name = textBox10.Text;
+                var con = Configuration.getInstance().getConnection();
+                //SqlCommand cmd = new SqlCommand("SELECT Company, Type, Name, ConversionalUnit AS Quantity, RetailPrice / CAST(ConversionalUnit AS decimal(10,2)) AS Price FROM Products WHERE Name LIKE '%' + @name + '%'", con);
+                SqlCommand cmd = new SqlCommand("SELECT * From Products WHERE Products.Name LIKE '%' + @name + '%'", con);
+                // SqlCommand cmd = new SqlCommand("SELECT Products.Company, Products.Type, Products.Name, Products.ConversionalUnit AS Quantity, Products.RetailPrice / CAST(Products.ConversionalUnit AS decimal(10,2)) AS Price, Stock.BatchNo FROM Products INNER JOIN Stock ON Products.ProductID = Stock.ProductID WHERE Products.Name LIKE '%' + @name + '%'", con);
+
+                cmd.Parameters.AddWithValue("@name", name);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                dataGridView1.DataSource = dt;
+                if (dt.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dt;
+
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridView1.AllowUserToAddRows = false;
+                    dataGridView1.AllowUserToDeleteRows = false;
+                    dataGridView1.AllowUserToOrderColumns = false;
+                    dataGridView1.AllowUserToResizeRows = false;
+                    dataGridView1.ReadOnly = true;
+                    dataGridView1.RowHeadersVisible = false;
+                    dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    dataGridView1.MultiSelect = false;
+                    dataGridView1.DefaultCellStyle.SelectionBackColor = Color.FromArgb(46, 53, 73);
+                    dataGridView1.DefaultCellStyle.SelectionForeColor = Color.White;
+                    dataGridView1.RowsDefaultCellStyle.BackColor = Color.White;
+                    dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(237, 243, 255);
+                    dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(46, 53, 73);
+                    dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9, FontStyle.Bold);
+
+                    DataGridViewButtonColumn editButton = new DataGridViewButtonColumn();
+                    editButton.HeaderText = "Add";
+                    editButton.Text = "Add";
+                    editButton.UseColumnTextForButtonValue = true;
+                    editButton.DefaultCellStyle.BackColor = Color.FromArgb(72, 123, 237);
+                    editButton.DefaultCellStyle.ForeColor = Color.White;
+                    editButton.DefaultCellStyle.Font = new Font("Tahoma", 9, FontStyle.Bold);
+                    dataGridView1.Columns.Add(editButton);
+                    dataGridView1.CellClick += dataGridView1_CellClick;
+                }
+                else
+                {
+                    MessageBox.Show("Product not found!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                // Fill the grid with data
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while fetching data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                SqlCommand cmd;
+                var con = Configuration.getInstance().getConnection();
+                cmd = new SqlCommand("insert into LOGS values (@CreatedAt , @LogTitle , @LogClass , @LogFunction)", con);
+                cmd.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+                cmd.Parameters.AddWithValue("@LogTitle", "AddStockinProducts _ GenericName");
+                cmd.Parameters.AddWithValue("@LogClass", "AddStockinProducts");
+                cmd.Parameters.AddWithValue("@LogFunction", "GenericName");
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void iconButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
